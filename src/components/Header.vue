e02d1633abaf15b8b23502bbb6800e5e8e1712ed
tony tolver <andrademaresantonio00@gmail.com>
2023-12-03 23:47:23 -0600

    ADD Detección de pagina de inicio

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,17 @@
 <script setup>
-   import { RouterLink } from 'vue-router';
+   import { computed } from 'vue';
+   import { RouterLink, useRoute } from 'vue-router';
+
+   const route = useRoute();
+
+   const paginaInicio = computed(() => route.name === 'inicio');
+
+
 </script>
 <template>
    <header
       class="bg-slate-800"
+      :class="{ 'header': paginaInicio }"
    > 
       <div class="mx-auto container px-5 py-16">
          <div class="flex justify-between items-center">
@@ -33,6 +41,7 @@
          </div>
          <form
             class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6"
+            v-if="paginaInicio"
          >
             <div class="space-y-4">
                <label
@@ -71,3 +80,11 @@
       </div>
    </header>
 </template>
+
+<style>
+   .header{
+      background-image: url('/img/bg.jpg');
+      background-size: cover;
+      background-position: center;
+   }
+</style>
